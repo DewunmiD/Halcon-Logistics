5927d5fa8a9608044d797a5387bd61fd6b751603
DewunmiD/DewunmiD <daewoorazor993.com>
2024-07-12 18:11:16 +0100

    new correction

--- src/MobileFooter.js
+++ src/MobileFooter.js
@@ -9,7 +9,7 @@ function MobileFooter() {
     <div className='md:hidden w-full !px-0 !mx-0 !overflow-hidden'>
     <div className="bg-black text-white flex justify-center items-center gap-2 mt-[40px]">
       
-      <ul className="text-left">
+      <ul className="text-left mt-4">
         <li className="mb-6 text-xl flex items-center gap-2">
           <FaPhoneAlt />: <span>+2348109883336</span>
         </li>
